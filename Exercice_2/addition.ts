bc21fc365b7fddf86a09af31d2f521d6e6d3ff8b
PierreD59 <pierre.dmt59@gmail.com>
2022-11-24 11:30:13 +0100

    Exercice 2
    Création d'une fonction qui additionne deux nombre en paramètre

---
+++ Exercice_2/addition.ts
@@ -0,0 +1,11 @@
+/**
+ * Fonction qui retourne l'addition de deux nombre en paramètre
+ * @param num1:number
+ * @param num2:number
+ * @returns num1 + num2
+ */
+function addition(num1:number, num2:number):number
+{
+    return num1 + num2;
+}
+console.log(addition(5,6));
